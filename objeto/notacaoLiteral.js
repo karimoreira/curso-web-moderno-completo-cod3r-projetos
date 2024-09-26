73df3361353875d670d5d28abcb5e29173d1cf6f
karimoreira <kari.moreira@cs.cruzeirodosul.edu.br>
2024-09-26 17:16:57 -0300

    feat: finalizando os meus exercícios/estudos em Objeto!!!

--- objeto/notacaoLiteral.js
+++ objeto/notacaoLiteral.js
@@ -4,4 +4,26 @@ const c = 3
 
 const obj1 = { a: a, b: b, c: c}
 const obj2 = { a, b, c}
-//parei aqui em notação literal
+console.log(obj1, obj2)
+
+
+const nomeAttr = 'nota'
+const valorAttr = 7.87
+
+const obj3 = {}
+obj3[nomeAttr] = valorAttr
+console.log(obj3)
+
+const obj4 = {[nomeAttr]: valorAttr}
+console.log(obj4)
+
+const obj5 = {
+    funcao1: function(){
+        // ...
+    },
+    funcao2(){
+        //..
+    }
+}
+
+console.log(obj5)
